5f341eff91a1a254a30ec190d1c48f391b3e9f74
Kyla <75227767+kylagelev@users.noreply.github.com>
2020-12-31 17:17:51 -0800

    code for net total

--- PyBank/main.py
+++ PyBank/main.py
@@ -11,6 +11,14 @@ with open(bankdata) as csvfile:
     count_months=csv_reader
     print(f'Number of Months: {len(count_months)}')
 
+#net total of profit/losses
+#want each item 1 from each row, make them into numbers and then add value
+    net_total = 0
+    for row in csv_reader:
+        profit_loss = int(row[1])
+        net_total = net_total + profit_loss
+    print(f'Net Total: ${net_total}')
+    
     for row in csv_reader:
         print(row)
 # %%
