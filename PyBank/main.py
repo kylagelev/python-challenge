8ce10de9eb41ef76a19a73a310636a09fe58f1eb
Kyla <75227767+kylagelev@users.noreply.github.com>
2020-12-31 17:09:16 -0800

    total months
    adding code to collect total months from all of dataset

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,8 +1,16 @@
+# %%
 import os
 import csv
 bankdata = os.path.join('..','Resources','budget_data.csv')
 with open(bankdata) as csvfile:
-    csv_reader=csv.reader(csvfile, delimiter=",")
+    csv_reader = csv.reader(csvfile, delimiter=",")
     header = next(csv_reader)
+
+    #total number of months, meaning total number of rows in data
+    csv_reader=list(csv_reader)
+    count_months=csv_reader
+    print(f'number of months: {len(count_months)}')
+    
     for row in csv_reader:
         print(row)
+# %%
