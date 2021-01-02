2322a44e4580f8ed327293d55cfa0766e3ab3d91
Kyla <75227767+kylagelev@users.noreply.github.com>
2021-01-02 15:19:45 -0800

    import csv file

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,8 @@
-
+import os
+import csv
+polldata = os.path.join('..','Resources','election_data.csv')
+with open(polldata) as csvfile:
+    csv_reader=csv.reader(csvfile, delimiter=",")
+    header = next(csv_reader)
+    for row in csv_reader:
+        print(row)
