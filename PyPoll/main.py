2e297defd2b15f2a21eccc152db39ccd64d5d20c
Kyla <75227767+kylagelev@users.noreply.github.com>
2021-01-04 16:46:07 -0800

    update PyPoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,11 +5,16 @@ polldata = os.path.join('..','Resources','election_data.csv')
 with open(polldata) as csvfile:
     csv_reader=csv.reader(csvfile, delimiter=",")
     header = next(csv_reader)
+    
+    print(f'Election Results')
+    print(f'-------------------')
 
-    #total number of votes cast
+#total number of votes cast
     candidates_list=list(csv_reader)
     number_of_votes = len(candidates_list)
     print(f'Total Votes: {number_of_votes}')
+    
+    print(f'-------------------')
     
 #candidates' individual votes and percentages
     candidates =[]
@@ -18,17 +23,15 @@ with open(polldata) as csvfile:
     vote_count_2 = 0
     vote_count_3 = 0
     
-    #collecting all names
     for row in candidates_list:
         name = row[2]
         candidates.append(name)
         
-    #collecting all list of unique names
+    #collected all list of unique names
     unique_names = []    
     for x in candidates:
         if x not in unique_names:
             unique_names.append(x)
-    print(unique_names)
         
     for row in candidates_list:
         name = row[2]
@@ -40,12 +43,36 @@ with open(polldata) as csvfile:
             vote_count_2 = vote_count_2 + 1
         if name == unique_names[3]:
             vote_count_3 = vote_count_3 + 1
-            
-    print (vote_count_0)
-    print (vote_count_1)
-    print (vote_count_2)
-    print (vote_count_3)
 
-
+    percentage_0 = (vote_count_0/number_of_votes)*100
+    percentage_1 = (vote_count_1/number_of_votes)*100
+    percentage_2 = (vote_count_2/number_of_votes)*100
+    percentage_3 = (vote_count_3/number_of_votes)*100
     
-# %%
+    print(f'{unique_names[0]}: {round(percentage_0,2)}00% ({vote_count_0})')
+    print(f'{unique_names[1]}: {round(percentage_1,2)}00% ({vote_count_1})')
+    print(f'{unique_names[2]}: {round(percentage_2,2)}00% ({vote_count_2})')
+    print(f'{unique_names[3]}: {round(percentage_3,2)}00% ({vote_count_3})')
+
+    print(f'-------------------')
+    
+#greatest votes = winner
+    individual_votes = []
+    individual_votes.append(vote_count_0)
+    individual_votes.append(vote_count_1)
+    individual_votes.append(vote_count_2)
+    individual_votes.append(vote_count_3)
+    
+    #want to order from greatest to least (descending order)
+    sorted_individual_votes = sorted(individual_votes, reverse = True)
+    
+    if sorted_individual_votes[0] == vote_count_0:
+        print(f'Winner: {unique_names[0]}!')
+    if sorted_individual_votes[0] == vote_count_1:
+        print(f'Winner: {unique_names[1]}!')
+    if sorted_individual_votes[0] == vote_count_2:
+        print(f'Winner: {unique_names[2]}!')
+    if sorted_individual_votes[0] == vote_count_3:
+        print(f'Winner: {unique_names[3]}!')
+
+#%%
